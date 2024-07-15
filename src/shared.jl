9a45da8ed4f4abfb905812e7563a2eeab7f78352
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2024-07-15 17:07:47 -0400

    Create shared.jl

---
+++ src/shared.jl
@@ -0,0 +1,13 @@
+# AbstractBinaryFloat, MemBits, ExpBits, SigBits, n_values,
+abstract type AbstractBinaryFloat{BitWidth, Precision} <: AbstractFloat end
+
+const ABF{W, P} = AbstractBinaryFloat{W, P} # AbstractBinaryFloat{Bitwidth, Precision}
+
+n_values(nbits) = 2^abs(nbits)
+
+MemBits(::Type{AbstractBinaryFloat{BitWidth, Precision}) = BitWidth
+Precision(::Type{AbstractBinaryFloat{BitWidth, Precision}) = Precision
+
+ExpBits(::Type{AbstractBinaryFloat{BitWidth, Precision}) = BitWidth - Precision
+
+

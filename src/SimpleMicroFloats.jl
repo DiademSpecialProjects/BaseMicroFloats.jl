a0e871785f99fd9eb3a7938e1ef46601bf671636
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2024-07-15 17:26:15 -0400

    Update SimpleMicroFloats.jl

--- src/SimpleMicroFloats.jl
+++ src/SimpleMicroFloats.jl
@@ -1,6 +1,7 @@
 module SimpleMicroFloats
 
 export AbstractBinaryFloat, bitwidth, nvalues,    # import all of these
+       fraction_bits,
        SimpleMicroFloat
 
 import Base: precision, exponent_bits, significand_bits, isbitstype

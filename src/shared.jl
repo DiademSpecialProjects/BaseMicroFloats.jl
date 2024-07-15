260318932d9574022e42cf1e1eb6d1325061326a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2024-07-15 17:28:02 -0400

    Update shared.jl

--- src/shared.jl
+++ src/shared.jl
@@ -3,7 +3,8 @@ abstract type AbstractBinaryFloat{BitWidth, Precision} <: AbstractFloat end
 const ABF{W, P} = AbstractBinaryFloat{W, P} # AbstractBinaryFloat{Bitwidth, Precision}
 
 nvalues(nbits) = 2^abs(nbits)
-
+bitsof(::Type{AbstractBinaryFloat{BitWidth, Precision}) = BitWidth
+  
 Base.length(::Type{AbstractBinaryFloat{BitWidth, Precision}) = BitWidth
 Base.precision(::Type{AbstractBinaryFloat{BitWidth, Precision}) = Precision
 

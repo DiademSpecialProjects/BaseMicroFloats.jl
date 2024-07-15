52792b23400e3a46b0840605791951ae3bfd0b49
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2024-07-15 17:25:40 -0400

    Update shared.jl

--- src/shared.jl
+++ src/shared.jl
@@ -1,13 +1,18 @@
-# AbstractBinaryFloat, MemBits, ExpBits, SigBits, n_values,
 abstract type AbstractBinaryFloat{BitWidth, Precision} <: AbstractFloat end
 
 const ABF{W, P} = AbstractBinaryFloat{W, P} # AbstractBinaryFloat{Bitwidth, Precision}
 
 nvalues(nbits) = 2^abs(nbits)
 
-bitwidth(::Type{AbstractBinaryFloat{BitWidth, Precision}) = BitWidth
+Base.length(::Type{AbstractBinaryFloat{BitWidth, Precision}) = BitWidth
 Base.precision(::Type{AbstractBinaryFloat{BitWidth, Precision}) = Precision
 
-expbits(::Type{AbstractBinaryFloat{BitWidth, Precision}) = BitWidth - Precision
-fracbits(::Type{AbstractBinaryFloat{BitWidth, Precision}) = Precision - 1
+Base.exponent_bits(::Type{AbstractBinaryFloat{BitWidth, Precision}) = BitWidth - Precision
+Base.significand_bits(::Type{AbstractBinaryFloat{BitWidth, Precision}) = Precision - 1
+
+Base.isbitstype(::Type{AbstractBinaryFloat{BitWidth, Precision}) = true
+
+bitwidth(::Type{AbstractBinaryFloat{BitWidth, Precision}) = BitWidth
+fraction_bits(::Type{AbstractBinaryFloat{BitWidth, Precision}) = Precision - 1
+
   

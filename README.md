00bc6e7164833aca1db4d770f33579ba4dd8875f
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-07-15 20:00:16 -0400

    model

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SimpleMicroFloats.jl
-The internal constructive model for MicroFloats.
+### The internal constructive model for MicroFloats.
+##### Copyright 2024 by Jeffrey Sarnoff
+
+[![Aqua QA](https://raw.githubusercontent.com/JuliaTesting/Aqua.jl/master/badge.svg)](https://github.com/JuliaTesting/Aqua.jl)  [![JET](https://img.shields.io/badge/%F0%9F%9B%A9%EF%B8%8F_tested_with-JET.jl-233f9a)](https://github.com/aviatesk/JET.jl)
--- src/model.jl
+++ src/model.jl
@@ -1,1 +1,19 @@
 
+function SimpleFloatModel(bitwidth, precision)
+    n_values = 2^bitwidth
+    values = (; n_values, bitwidth, precision)
+
+    n_exponent_bits = bitwidth - precision 
+    n_exponents = 2^n_exponent_bits
+    n_cycles = cld(n_values, n_exponents)
+    exponents = (; n_exponents, n_cycles)
+
+    n_fraction_bits = precision - 1
+    n_fractions = 2^n_fraction_bits
+    n_cycles = cld(n_values, n_fractions)
+    fractions = (; n_fractions, n_cycles)
+
+    (; values, exponents, fractions)
+end
+
+    

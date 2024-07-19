a6951f8c37f8c45b622b74c578b55c1110f4a775
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-07-19 12:36:19 -0400

    fixup

--- src/type.jl
+++ src/type.jl
@@ -5,6 +5,6 @@ end
 
 function SimpleFloat(bitwidth, precision)
     values = map(μValue, construct_SimpleFloat(bitwidth, precision))
-    codings = map(μEncode, 0:2^(bitwidth-1))
+    codings = map(μEncode, 0:((2^bitwidth)-1))
     SimpleFloat{bitwidth, precision}(values, codings)
 end

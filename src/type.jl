803c260ba17e2dd77479cdb3b56a513b2fa526f3
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-07-19 12:45:49 -0400

    struct

--- src/type.jl
+++ src/type.jl
@@ -1,10 +1,16 @@
-struct SimpleFloat{BitWidth, Precision} <:  AbstractSimpleFloat{BitWidth, Precision}
+struct SimpleMicroFloat{BitWidth, Precision} <:  AbstractSimpleFloat{BitWidth, Precision}
     values::Vector{μValue}
     codings::Vector{μEncode}
 end
 
-function SimpleFloat(bitwidth, precision)
+function SimpleMicroFloat(bitwidth, precision)
     values = map(μValue, construct_SimpleFloat(bitwidth, precision))
     codings = map(μEncode, 0:((2^bitwidth)-1))
-    SimpleFloat{bitwidth, precision}(values, codings)
+    SimpleMicroFloat{bitwidth, precision}(values, codings)
 end
+
+struct SimpleFloat{BitWidth, Precision} <: AbstractSimpleFloat{BitWidth, Precision}
+    value::μValue
+    coding::μEncode
+end
+

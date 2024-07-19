70e2857be45cd9e8b2e554df298baef1d146440a
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-07-19 11:58:29 -0400

    simplify

--- src/construct.jl
+++ src/construct.jl
@@ -1,30 +1,31 @@
-#   model: (; bitwidth, precision, n_sign_bits, n_exponent_bits, n_fraction_bits,
-#             n_sign_values, n_exponent_values, n_fraction_values,
-#             n_exponent_cycles, n_fraction_cycles)
+#= 
+model = (; bitwidth, precision, n_values, 
+           n_exponent_bits, n_exponent_values, n_exponent_cycles,
+           n_fraction_bits, n_fraction_values, n_fraction_cycles)
+=#
 
 function construct_simplefloat(bitwidth, precision)
-    model = SimpleFloatModel(bitwidth, precision)
-    fractions = construct_fraction_series(model.n_fraction_values, model.n_fraction_cycles)
-    bias = exponent_bias(bitwidth, precision)
-    exponents = construct_exponent_series(model.n_exponent_values, model.n_exponent_cycles)
-
+    model = characterize_SimpleFloat(bitwidth, precision)
+    significands = construct_significand_series(model)
+    exponents = construct_exponent_series(model)
+    significands .* exponents
 end
 
-function construct_fraction_series(n_values, n_cycles)
-    fraction_sequence = (0:n_values-1) .// n_values
-    mixed_fraction_sequence = 1 .+ fraction_sequence
-    mixed_fraction_series = Iterators.flatten(fill(mixed_fraction_sequence, n_cycles-1))
-    append!(fraction_sequence, mixed_fraction_series)
-    fraction_sequence
+function construct_significand_series(model)
+    fraction_sequence = (0:model.n_fraction_values-1) .// model.n_values
+    normal_sequence = 1 .+ fraction_sequence
+    append!(fraction_sequence, repeat(normal_sequence, model.n_fraction_cycles - 1))
 end
 
-function construct_exponent_series(n_values, n_cycles)
-    bias = (n_values >> 1) - 1
-    raw_exponent_sequence = (0:n_values-1) 
-    biased_exponent_sequence = raw_exponent_sequence .- bias
-    series = 
-    mixed_fraction_series = Iterators.flatten(fill(mixed_fraction_sequence, n_cycles-1))
-    append!(fraction_sequence, mixed_fraction_series)
-    fraction_sequence
-    exponent_sequence = construct_exponent_sequence(n_values)
+function construct_exponent_series(model)
+    biased_exponent_series = construct_biased_exponent_series(model)
+    map(x->2.0^x, biased_exponent_series)
+end
+
+function construct_biased_exponent_series(model)
+    bias = (model.n_exponent_values >> 1)
+    raw_exponent_sequence = (0:model.n_exponent_values-1) 
+    biased_exponent_sequence = collect(raw_exponent_sequence .- bias)
+    biased_exponent_sequence[1] = biased_exponent_sequence[2] # exponent for subnormals is min exponent for normals
+    collect(Iterators.flatten(map(x->fill(x, model.n_fraction_values), biased_exponent_sequence)))
 end
--- src/model.jl
+++ src/model.jl
@@ -1,21 +1,21 @@
-function SimpleFloatModel(bitwidth, precision)
+function characterize_SimpleFloat(bitwidth, precision)
     n_values = 2^bitwidth
 
-    n_sign_bits = 0
-    n_sign_values = 2 * n_sign_bits
-
-    n_exponent_bits = bitwidth - precision + 1
-    n_exponent_values = 2^n_exponent_bits
-    n_exponent_cycles = cld(n_values, n_exponent_values)
-
-    n_fraction_bits = bitwidth - n_exponent_bits - n_sign_bits 
+    n_fraction_bits = precision - 1 
     n_fraction_values = 2^n_fraction_bits
-    n_fraction_cycles = cld(n_values, n_fraction_values)
+    n_fraction_cycles = div(n_values, n_fraction_values)
+    @assert n_fraction_cycles * n_fraction_values == n_values
+    
+    n_exponent_values = n_fraction_cycles
+    n_exponent_cycles = n_fraction_values
 
-    (; bitwidth, precision, n_values, n_sign_bits, n_exponent_bits, n_fraction_bits,
-                            n_sign_values, n_exponent_values, n_fraction_values,
-                            n_exponent_cycles, n_fraction_cycles)
+    n_exponent_bits = bitwidth - n_fraction_bits # there is no sign bit
+
+    (; bitwidth, precision, n_values, 
+       n_exponent_bits, n_exponent_values, n_exponent_cycles,
+       n_fraction_bits, n_fraction_values, n_fraction_cycles)
 end
+
 
 #=
 n_fraction_cycles * n_fraction_values == n_exponent_cycles * n_exponent_values == n_values
@@ -23,10 +23,3 @@ n_fraction_cycles * n_fraction_values == n_exponent_cycles * n_exponent_values =
 n_fraction_values == n_exponent_cycles
 n_fraction_cycles == n_exponent_values
 =#
-
-min_raw_exponent = 0
-max_raw_exponent = n_exponent_values - 1
-raw_exponent_sequence = min_raw_exponent:max_raw_exponent
-bias = 2^(n_exponent_values-1)
-biased_exponent_sequence = raw_exponent_sequence .- bias
-exponent_sequence = 2.0 .^ biased_exponent_sequence

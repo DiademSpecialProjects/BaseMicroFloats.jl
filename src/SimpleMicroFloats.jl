630f509b7fdb36056065b9eeed5cae0ed14cf689
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-07-19 12:10:27 -0400

    cleanup exports

--- src/SimpleMicroFloats.jl
+++ src/SimpleMicroFloats.jl
@@ -4,9 +4,7 @@
 
 module SimpleMicroFloats
 
-export AbstractMicroFloat, 
-       n_bits, n_sign_bits, n_exponent_bits, n_fraction_bits,
-       n_values, n_sign_values, n_exponent_values, n_fraction_values, 
+export AbstractMicroFloat,
        μEncode, μValue, μSafeEncode, μSafeValue,
        SimpleMicroFloat
 

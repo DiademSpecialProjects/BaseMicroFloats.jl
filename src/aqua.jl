d34bda4b5f7467442126f376a70b24f649b95a27
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-07-26 00:03:59 -0400

    simplify

--- src/aqua.jl
+++ src/aqua.jl
@@ -1,6 +1,4 @@
 # defs follow suggestions from Aqua to resolve ambiguities
 
-# SimpleMicroFloat{BitWidth, Precision}(x::Real, rounding::RoundingMode) where {BitWidth, Precision} = false
-# SimpleFloat{BitWidth, Precision}(x::Real, rounding::RoundingMode) where {BitWidth, Precision} = false
 SimpleMicroFloat(::Real, ::RoundingMode) = nothing
 SimpleFloat(::Real, ::RoundingMode) = nothing

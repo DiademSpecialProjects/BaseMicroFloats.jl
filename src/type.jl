30dcaaae9cdd419ceb787c97d975688b00406201
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-07-25 22:38:27 -0400

    simplify

--- src/type.jl
+++ src/type.jl
@@ -15,7 +15,7 @@ struct SimpleFloat{Bits, Prec} <: AkoSimpleMicroFloat{Bits, Prec}
     code::C where {C<:Union{UInt8, UInt16}}
     value::V where {V<:Union{Float32, Float64}}
 
-    function SimpleFloat{Bits, Prec}(code::T, value::Real) where {T<:Union{UInt8, UInt16}}
+    function SimpleFloat(Bits, Prec, code::T, value::Real) where {T<:Union{UInt8, UInt16}}
         V = T == UInt8 ? Float32 : Float64
         return new{Bits, Prec}(code, T(value))
     end

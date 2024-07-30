135a6ef2ae683bc297c683363fc11edab8b8f74f
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-07-29 21:15:44 -0400

    show

--- src/show.jl
+++ src/show.jl
@@ -1,3 +1,24 @@
+function show_consistent_rationals(encoding, values)
+    enc = hexstr.(encoding)
+    vals = consistent_rationals(values)
+    z = zip(enc, vals)
+    s = """\n"""
+    for i in z                                                                                                             
+      s = s * (string("\n",i[1],"\t",i[2]))                                                                            
+    end
+    s * "\n"
+end
+
+# show_consistent_rationals(encoding(SMF87),vcat(0, diff( values(SMF87))))
+
+function hexstr(x)
+  s = @sprintf("%x", x)
+  if x < 0x10
+    s = "0" * s
+  end
+  "0x" * s
+end
+
 function consistent_rationals(xs)
   map((n,d)->string(n,"/",d), consistent_numers(xs), fill(maxdenom(xs), length(xs)))
 end

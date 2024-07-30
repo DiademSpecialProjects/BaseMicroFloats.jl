25ffdb191cd913f9a4e70cca1f7055148e06d0bd
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-07-29 21:58:54 -0400

    consistent naming

--- src/type.jl
+++ src/type.jl
@@ -1,20 +1,20 @@
-struct SimpleMicroFloat{Bits, Prec} <: AkoSimpleMicroFloat{Bits, Prec}
+struct SimpleMicroFloat{BitWidth, Prec} <: AkoSimpleMicroFloat{BitWidth, Prec}
     encoding::Vector{T} where {T<:Union{UInt8, UInt16}}
     values::Vector{T} where {T<:Union{Float32, Float64}}
 
-    function SimpleMicroFloat(Bits, Prec)
-        encoding = SMF_encoding(Bits, Prec)
-        values = SMF_values(Bits, Prec)
-        new{Bits, Prec}(encoding, values)
+    function SimpleMicroFloat(BitWidth, Prec)
+        encoding = SMF_encoding(BitWidth, Prec)
+        values = SMF_values(BitWidth, Prec)
+        new{BitWidth, Prec}(encoding, values)
     end
 end
 
 encoding(x::SimpleMicroFloat) = x.encoding
 Base.values(x::SimpleMicroFloat) = x.values
 
-bitwidth(::Type{SimpleMicroFloat{Bitwidth, Precision}}) where {Bitwidth, Precision} = Bitwidth
-Base.precision(::Type{SimpleMicroFloat{Bitwidth, Precision}}) where {Bitwidth, Precision} = Precision
+bitwidth(::Type{SimpleMicroFloat{BitWidth, Precision}}) where {BitWidth, Precision} = BitWidth
+Base.precision(::Type{SimpleMicroFloat{BitWidth, Precision}}) where {BitWidth, Precision} = Precision
 
-bitwidth(x::SimpleMicroFloat{Bitwidth, Precision}) where {Bitwidth, Precision} = bitwidth(typeof(x))
-Base.precision(x::SimpleMicroFloat{Bitwidth, Precision}) where {Bitwidth, Precision} = precision(typeof(x))
+bitwidth(x::SimpleMicroFloat{BitWidth, Precision}) where {BitWidth, Precision} = bitwidth(typeof(x))
+Base.precision(x::SimpleMicroFloat{BitWidth, Precision}) where {BitWidth, Precision} = precision(typeof(x))
 

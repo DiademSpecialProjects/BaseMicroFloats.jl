0acf56b12a68ce1279c6a8f6027b3ab1fed1f0ce
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-08-06 03:39:32 -0400

    tests

--- test/intraconvert.jl
+++ test/intraconvert.jl
@@ -2,20 +2,49 @@ using BaseMicroFloats, Test
 
 const FLOAT = Union{Float64, Float32, Float16}
 
+MF11 = BaseMicroFloat(1,1);
+MF21 = BaseMicroFloat(2,1);
+MF22 = BaseMicroFloat(2,2);
 MF31 = BaseMicroFloat(3,1);
 MF32 = BaseMicroFloat(3,2);
+MF33 = BaseMicroFloat(3,3);
 MF41 = BaseMicroFloat(4,1);
 MF42 = BaseMicroFloat(4,2);
 MF43 = BaseMicroFloat(4,3);
+MF44 = BaseMicroFloat(4,4);
 MF51 = BaseMicroFloat(5,1);
 MF52 = BaseMicroFloat(5,2);
 MF53 = BaseMicroFloat(5,3);
 MF54 = BaseMicroFloat(5,4);
+MF55 = BaseMicroFloat(5,5);
 MF61 = BaseMicroFloat(6,1);
 MF62 = BaseMicroFloat(6,2);
 MF63 = BaseMicroFloat(6,3);
 MF64 = BaseMicroFloat(6,4);
 MF65 = BaseMicroFloat(6,5);
+MF66 = BaseMicroFloat(6,6);
+MF71 = BaseMicroFloat(7,1);
+MF72 = BaseMicroFloat(7,2);
+MF73 = BaseMicroFloat(7,3);
+MF74 = BaseMicroFloat(7,4);
+MF75 = BaseMicroFloat(7,5);
+MF76 = BaseMicroFloat(7,6);
+MF77 = BaseMicroFloat(7,7);
+MF81 = BaseMicroFloat(8,1);
+MF82 = BaseMicroFloat(8,2);
+MF83 = BaseMicroFloat(8,3);
+MF84 = BaseMicroFloat(8,4);
+MF85 = BaseMicroFloat(8,5);
+MF86 = BaseMicroFloat(8,6);
+MF87 = BaseMicroFloat(8,7);
+
+M71 = values(MF71);
+M72 = values(MF72);
+M73 = values(MF73);
+M74 = values(MF74);
+M75 = values(MF75);
+M76 = values(MF76);
+M77 = values(MF77);
 
 M32idx3 = values(MF32)[3];
 M32idx4 = values(MF52)[4];
